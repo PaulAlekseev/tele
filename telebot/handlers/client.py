cfd2cd581c64ed370829595115561249dbf5fd75
dante <pavel.zommerfeld@yandex.ru>
2022-10-14 18:26:13 +0300

    init

--- telebot/handlers/client.py
+++ telebot/handlers/client.py
@@ -4,10 +4,7 @@ from bot import bot
 
 
 async def answer(message: types.Message):
-    await bot.send_message(message.from_user.id, 'cm')
-    a = await message.document.get_file()
-    print(await a.get_url())
-    print(message.document.file_id)
+    await bot.send_message(message.from_user.id, 'shut yo bitch ass')
 
 
 def register_handlers_client(db: Dispatcher):
