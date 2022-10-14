e7cc291fc5268ba2648d91df2a0bff6163160199
dante <pavel.zommerfeld@yandex.ru>
2022-10-14 19:13:28 +0300

    init2

--- telebot/handlers/client.py
+++ telebot/handlers/client.py
@@ -5,6 +5,7 @@ from bot import bot
 
 async def answer(message: types.Message):
     await bot.send_message(message.from_user.id, 'shut yo bitch ass')
+    await message.delete()
 
 
 def register_handlers_client(db: Dispatcher):
