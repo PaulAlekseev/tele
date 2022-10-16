0b2b17ce30dd63160cd53fc42c39f6f0c24fbf38
dante <pavel.zommerfeld@yandex.ru>
2022-10-16 18:59:46 +0300

    init2dasd

--- telebot/bot.py
+++ telebot/bot.py
@@ -1,6 +1,8 @@
+import os
+
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
-bot = Bot('5782290576:AAE9t4Y0HFAt5hvaygrnwA2JI7D2aRlpdaE')
+bot = Bot(os.getenv('TOKEN'))
 db = Dispatcher(bot)
