db2d54d0ce2d3f56dd6e90ffc4c29806db1c499f
dante <pavel.zommerfeld@yandex.ru>
2022-10-16 21:27:11 +0300

    init2dddasddasdsaddaddasddasdaasd

--- telebot/celery_tasks/tasks.py
+++ telebot/celery_tasks/tasks.py
@@ -6,8 +6,9 @@ from celery import Celery
 app = Celery('celery_worker', broker='redis://redis:6379/0')
 
 
-@app.task(bind=True)
-def add():
+@app.task
+def add(x, y):
+    print(x, y)
     time.sleep(5)
     print('bruh')
 
--- telebot/handlers/client.py
+++ telebot/handlers/client.py
@@ -7,7 +7,7 @@ from celery_tasks.tasks import add
 async def answer(message: types.Message):
     await bot.send_message(message.from_user.id, 'shut yo bitch ass')
     await bot.send_message(message.from_user.id, 'You should end your live NOW')
-    add.delay()
+    add.delay(1, 2)
     await message.delete()
 
 
