0119fd9c63a7bd67fa17f770fa2097fc817bdaa7
dante <pavel.zommerfeld@yandex.ru>
2022-10-16 22:22:31 +0300

    adas

--- telebot/celery_tasks/tasks.py
+++ telebot/celery_tasks/tasks.py
@@ -3,7 +3,7 @@ import time
 from celery import Celery
 
 
-app = Celery('celery_worker', broker='redis://redis:6379/0')
+app = Celery('celery_worker', broker='redis://redis:6379/0', include=['app.tasks'])
 
 
 @app.task
