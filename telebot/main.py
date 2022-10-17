84cfd84c2e56de0d65ae7b8bc72502143207b266
dante <pavel.zommerfeld@yandex.ru>
2022-10-17 22:06:37 +0300

    adasd

--- telebot/main.py
+++ telebot/main.py
@@ -3,8 +3,11 @@ from aiogram.utils import executor
 from bot import db
 
 from handlers.client import register_handlers_client
+from entities.db.db_engine import engine
+from entities.db.db_tables import Base
 
 
+Base.metadata.create_all(engine)
 register_handlers_client(db)
 
 executor.start_polling(db, skip_updates=True)
