6aaa2b647fdfb38701a7823ee9cafe1f97d660b8
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-10-18 22:45:48 +0300

    hello

--- telebot/entities/async_db/db_engine.py
+++ telebot/entities/async_db/db_engine.py
@@ -4,6 +4,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-engine = create_async_engine(os.getenv('DB_STRING'))
+engine = create_async_engine(os.getenv('ASYNC_DB_STRING'))
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
