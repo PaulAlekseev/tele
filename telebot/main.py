83d3fb3117861a89392e1f5b8b684b85458f9d72
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-10-18 23:06:12 +0300

    hello

--- telebot/main.py
+++ telebot/main.py
@@ -1,8 +1,12 @@
 from aiogram.utils import executor
 
 from bot import db
+from entities.db.db_engine import engine
 
 from handlers.client import register_handlers_client
+from entities.db.db_tables import Base
+
+Base.metadata.create_all(engine)
 
 
 register_handlers_client(db)
