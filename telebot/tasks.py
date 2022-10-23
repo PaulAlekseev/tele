077c94e03034304dc2dbbfd4d01ed8d291f4addc
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-10-23 23:09:00 +0300

    hello

--- telebot/tasks.py
+++ telebot/tasks.py
@@ -2,13 +2,11 @@ import asyncio
 import multiprocessing
 import os
 import time
-from concurrent.futures import ThreadPoolExecutor
 
 from bot import bot
 from celery_app import app
-from entities.async_db.db_specifications import ScanIdSpecification
 from entities.db.db_repos import CredentialsRepository, ScanRepository
-from .entities.functions import get_file_credentials
+from entities.functions import get_file_credentials
 
 
 @app.task
