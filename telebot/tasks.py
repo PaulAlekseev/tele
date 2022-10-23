56e2c17e6f08ad6ee3fc03c4ab62f4d13579f007
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-10-23 22:20:26 +0300

    hello

--- telebot/tasks.py
+++ telebot/tasks.py
@@ -22,7 +22,7 @@ def validate(scan_id: int, user_id):
     sync_send_message(message=f"Your scan {scan_id}, has been started", chat_id=user_id)
     # Getting data from document
     scan_repo = ScanRepository()
-    scan = scan_repo.get(
+    scan = scan_repo.get_by_id(
         ScanIdSpecification(scan_id=scan_id)
     )
     sync_send_message(message=scan, chat_id=user_id)
