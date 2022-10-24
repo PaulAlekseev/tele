b7b050228b7a8696bad0db27e1fdaac3bbfc8203
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-10-25 00:35:15 +0300

    hello

--- telebot/main.py
+++ telebot/main.py
@@ -1,6 +1,6 @@
 from aiogram.utils import executor
 
-from bot import db
+from bot import dp
 from entities.db.db_engine import engine
 
 from handlers.client import register_handlers_client
@@ -9,7 +9,7 @@ from entities.db.db_tables import Base
 Base.metadata.create_all(engine)
 
 
-register_handlers_client(db)
+register_handlers_client(dp)
 
-executor.start_polling(db, skip_updates=True)
+executor.start_polling(dp, skip_updates=True)
 
