b9b6b7209dd15ba9ceea99f2d0f2356e676d5b09
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-06 02:55:32 +0300

    helloNew

--- telebot/bot.py
+++ telebot/bot.py
@@ -4,5 +4,15 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
+# webhook settings
+WEBHOOK_HOST = 'https://awshosttest.site/'
+WEBHOOK_PATH = ''
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 3001
+
+
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
--- telebot/main.py
+++ telebot/main.py
@@ -1,6 +1,8 @@
+from aiogram.dispatcher.webhook import DEFAULT_ROUTE_NAME
 from aiogram.utils import executor
+from aiogram.utils.executor import set_webhook
 
-from bot import dp
+from bot import dp, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from entities.db.db_engine import engine
 from handlers.admin import register_handlers_admin
 from handlers.client import register_handlers_client
@@ -12,5 +14,14 @@ Base.metadata.create_all(engine)
 register_handlers_client(dp)
 register_handlers_admin(dp)
 
-executor.start_polling(dp, skip_updates=True)
 
+if __name__ == '__main__':
+    executor = set_webhook(dispatcher=dp,
+                           webhook_path=WEBHOOK_PATH,
+                           loop=None,
+                           skip_updates=None,
+                           check_ip=False,
+                           retry_after=None,
+                           route_name=DEFAULT_ROUTE_NAME,
+                           )
+    executor.run_app(host=WEBAPP_HOST, port=WEBAPP_PORT)
