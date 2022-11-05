dbd0e99545c6771a214545eb8bbb975ae47220ac
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-05 07:01:45 +0300

    hello

--- telebot/handlers/admin.py
+++ telebot/handlers/admin.py
@@ -114,7 +114,7 @@ async def change_activation_type_active(message: types.Message, regexp, activity
                 int(regexp.group(1))
             ))
             if len(activation_types) == 0:
-                bot.send_message(message.from_user.id, f"There is no activation type with id - {regexp.group(1)}")
+                await bot.send_message(message.from_user.id, f"There is no activation type with id - {regexp.group(1)}")
                 return 0
             activation_type = activation_types[0]
             activation_type.active = activity
