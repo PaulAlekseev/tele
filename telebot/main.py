b29c6b062f863a88f84dbb9cfe82a9ead29ade67
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-06 16:39:18 +0300

    helloNew

--- telebot/main.py
+++ telebot/main.py
@@ -1,5 +1,5 @@
 from aiogram.dispatcher.webhook import DEFAULT_ROUTE_NAME
-from aiogram.utils.executor import set_webhook
+from aiogram.utils.executor import set_webhook, start_webhook
 
 from bot import dp, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT, bot, WEBHOOK_URL
 from entities.db.db_engine import engine
@@ -28,15 +28,24 @@ async def on_shutdown(dp):
     await dp.storage.wait_closed()
 
 
-if __name__ == '__main__':
-    executor = set_webhook(dispatcher=dp,
-                           webhook_path=WEBHOOK_PATH,
-                           loop=None,
-                           skip_updates=None,
-                           check_ip=False,
-                           on_startup=on_startup,
-                           on_shutdown=on_shutdown,
-                           retry_after=None,
-                           route_name=DEFAULT_ROUTE_NAME,
-                           )
-    executor.run_app(host=WEBAPP_HOST, port=WEBAPP_PORT)
+# executor = set_webhook(dispatcher=dp,
+#                        webhook_path=WEBHOOK_PATH,
+#                        loop=None,
+#                        skip_updates=None,
+#                        check_ip=False,
+#                        on_startup=on_startup,
+#                        on_shutdown=on_shutdown,
+#                        retry_after=None,
+#                        route_name=DEFAULT_ROUTE_NAME,
+#                        )
+# executor.run_app(host=WEBAPP_HOST, port=WEBAPP_PORT)
+
+start_webhook(
+    dispatcher=dp,
+    webhook_path=WEBHOOK_PATH,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host=WEBAPP_HOST,
+    port=WEBAPP_PORT,
+)
