1302470a3df8b170b704a3651533bd901e3ff8ad
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-06 16:13:38 +0300

    helloNew

--- telebot/bot.py
+++ telebot/bot.py
@@ -10,8 +10,8 @@ WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'  # or ip
-WEBAPP_PORT = 2001
+WEBAPP_HOST = '127.0.0.1'  # or ip
+WEBAPP_PORT = 3001
 
 
 bot = Bot(os.getenv('TOKEN'))
