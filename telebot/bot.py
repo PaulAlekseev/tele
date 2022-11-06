b4fded89af703745dfe9f70fba4e722c6930848f
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-06 13:38:51 +0300

    helloNew

--- telebot/bot.py
+++ telebot/bot.py
@@ -2,7 +2,8 @@ import os
 
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-
+from aiogram.dispatcher.webhook import DEFAULT_ROUTE_NAME
+from aiogram.utils.executor import set_webhook
 
 # webhook settings
 WEBHOOK_HOST = 'https://awshosttest.site/'
@@ -11,8 +12,19 @@ WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
 WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3000
+WEBAPP_PORT = 3001
 
 
 bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot)
+
+if __name__ == '__main__':
+    executor = set_webhook(dispatcher=dp,
+                           webhook_path=WEBHOOK_PATH,
+                           loop=None,
+                           skip_updates=None,
+                           check_ip=False,
+                           retry_after=None,
+                           route_name=DEFAULT_ROUTE_NAME,
+                           )
+    executor.run_app(host=WEBAPP_HOST, port=WEBAPP_PORT)
