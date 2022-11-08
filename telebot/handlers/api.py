db2f9bbf7e919c375ca2a56b9bcefa0004a0d6bd
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-09 00:16:01 +0300

    helloNew

--- telebot/handlers/api.py
+++ telebot/handlers/api.py
@@ -8,18 +8,21 @@ from bot import bot
 
 
 async def handle_notify(request: BaseRequest):
-    post = await request.post()
-    invoice_status = post.get('status')
-    tele_id = post.get('custom_data1')
-    custom_data = post.get('custom_data2')
-    if invoice_status == 'paid':
-        await bot.send_message(
-            chat_id=tele_id,
-            text='paid lol'
-        )
-    if invoice_status:
-        await bot.send_message(tele_id, f'''Your invoice status has been updated:
-{invoice_status}''')
+    try:
+        post = await request.post()
+        invoice_status = post.get('status')
+        tele_id = post.get('custom_data1')
+        custom_data = post.get('custom_data2')
+        if invoice_status == 'paid':
+            await bot.send_message(
+                chat_id=tele_id,
+                text='paid lol'
+            )
+        if invoice_status:
+            await bot.send_message(tele_id, f'''Your invoice status has been updated:
+    {invoice_status}''')
+    except Exception:
+        pass
     return Response(text='hello', status=200)
 
 
