351c06d3abc8d37bef3e1961633e199faa846b2d
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-08 20:05:47 +0300

    helloNew

--- telebot/bot.py
+++ telebot/bot.py
@@ -5,12 +5,12 @@ from aiogram.dispatcher import Dispatcher
 
 
 # webhook settings
-WEBHOOK_HOST = 'https://awshosttest.site/'
+WEBHOOK_HOST = 'https://osakatokyo.online/'
 WEBHOOK_PATH = ''
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '127.0.0.1'  # or ip
+WEBAPP_HOST = 'localhost'  # or ip
 WEBAPP_PORT = 3001
 
 
--- telebot/main.py
+++ telebot/main.py
@@ -40,13 +40,13 @@ async def on_shutdown(dp):
 #                        )
 # executor.run_app(host=WEBAPP_HOST, port=WEBAPP_PORT)
 
-# start_webhook(
-#     dispatcher=dp,
-#     webhook_path=WEBHOOK_PATH,
-#     on_startup=on_startup,
-#     on_shutdown=on_shutdown,
-#     skip_updates=True,
-#     host=WEBAPP_HOST,
-#     port=WEBAPP_PORT,
-# )
-start_polling(dispatcher=dp, skip_updates=True)
+start_webhook(
+    dispatcher=dp,
+    webhook_path=WEBHOOK_PATH,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    host=WEBAPP_HOST,
+    port=WEBAPP_PORT,
+)
+# start_polling(dispatcher=dp, skip_updates=True)
