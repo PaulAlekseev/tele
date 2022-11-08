8cc4f57012c9b2ae076d594fc18c0b96dc70f12b
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-08 23:02:05 +0300

    helloNew

---
+++ telebot/handlers/api.py
@@ -0,0 +1,29 @@
+import os
+
+from aiohttp.abc import BaseRequest
+from aiohttp import web
+from aiohttp.web_response import Response
+
+from bot import bot
+
+
+async def handle_notify(request: BaseRequest):
+    post = await request.post()
+    invoice_status = post.get('status')
+    tele_id = post.get('custom_data1')
+    custom_data = post.get('custom_data2')
+    if invoice_status == 'paid':
+        print('paid')
+    if invoice_status:
+        await bot.send_message(tele_id, f'''Your invoice status has been updated:
+{invoice_status}''')
+    print('tele_id: ', tele_id)
+    print('custom_data: ', custom_data)
+    return Response(text='hello', status=200)
+
+
+routes = [
+    web.post(f"/api/{os.getenv('TOKEN')}/payment_notify", handle_notify)
+]
+app = web.Application()
+app.add_routes()
---
+++ telebot/other/invoice.py
@@ -0,0 +1,47 @@
+import json
+import os
+
+import aiohttp
+
+
+class Invoice:
+    def __init__(
+            self,
+            amount: str,
+            session: aiohttp.ClientSession,
+            token_name: str,
+            tele_id: str,
+            custom_data: str,
+            api_key: str,
+            password: str,
+    ):
+        self._url = f'https://coinremitter.com/api/v3/{token_name}/create-invoice'
+        self._body = {
+            'api_key': api_key,
+            'password': password,
+            'currency': 'USD',
+            'amount': amount,
+            'notify_url': f"{os.getenv('WEBHOOK_HOST')}api/{os.getenv('TOKEN')}/notify",
+            'custom_data1': tele_id,
+            'custom_data2': custom_data
+        }
+        self._headers = {
+            'Accept': 'application/json',
+        }
+        self._session = session
+        self._response = None
+
+    async def create_invoice(self, ):
+        async with self._session.post(
+                url=self._url, data=self._body, headers=self._headers
+        ) as response:
+            result = json.loads(await response.text())
+            self._response = result
+            print(self._response)
+            return self._response
+
+    async def get_address(self):
+        return self._response['data']['address']
+
+    async def get_url(self):
+        return self._response['data']['url']
