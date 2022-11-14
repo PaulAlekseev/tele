b81634572b13453e6ca01b6532763dafa0ec5438
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-14 22:06:20 +0300

    helloNew

--- telebot/handlers/admin.py
+++ telebot/handlers/admin.py
@@ -1,5 +1,4 @@
 import datetime
-from datetime import datetime, date
 
 from aiogram import types, Dispatcher
 from aiogram.types import InputFile, Message
@@ -21,7 +20,7 @@ async def get_by(credential_result, message):
         return 0
     text_result = form_credentials_admin(credential_result)
     text_file = InputFile(
-        path_or_bytesio=text_result, filename=f'{datetime.now()}-{message.from_user.id}.txt'
+        path_or_bytesio=text_result, filename=f'{datetime.date.today()}-{message.from_user.id}.txt'
     )
     await bot.send_document(
         chat_id=message.from_user.id,
@@ -34,8 +33,8 @@ async def get_by_date(message: Message, regexp):
     if message.from_user.id not in IDS:
         return 0
     try:
-        date1 = datetime.strptime(regexp.group(1), '%d.%m.%Y').date()
-        date2 = datetime.strptime(regexp.group(2), '%d.%m.%Y').date()
+        date1 = datetime.datetime.strptime(regexp.group(1), '%d.%m.%Y').date()
+        date2 = datetime.datetime.strptime(regexp.group(2), '%d.%m.%Y').date()
     except ValueError:
         await bot.send_message(message.from_user.id, 'Enter valid dates')
         return 0
@@ -71,7 +70,7 @@ async def get_statistics(message: types.Message):
                 await bot.send_message(message.from_user.id, 'No users')
                 return 0
             string = form_user_statistics(users)
-            text_file = InputFile(path_or_bytesio=string, filename=f'User statistics - {date.today()}.txt')
+            text_file = InputFile(path_or_bytesio=string, filename=f'User statistics - {datetime.date.today()}.txt')
             await bot.send_document(
                 chat_id=message.from_user.id,
                 document=text_file,
