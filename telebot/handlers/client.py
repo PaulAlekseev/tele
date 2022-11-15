788504e67d862139a30242df24682b8e0abc40e7
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-16 00:38:44 +0300

    helloNew

--- telebot/handlers/client.py
+++ telebot/handlers/client.py
@@ -57,7 +57,7 @@ async def profile(message: types.Message):
     async with async_session() as session:
         async with session.begin():
             activation_repo = AIOActivationRepo(session)
-            latest_activation = await activation_repo.get_latest(message.from_user.id)
+            latest_activation = await activation_repo.get_latest(str(message.from_user.id))
             activation_exist = True if latest_activation else False
             active = True if activation_exist and latest_activation.expires >= datetime.date.today() else False
             text_dict = profile_text[emoji.demojize(message.text)]
