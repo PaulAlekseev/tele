93f74d90651c4d0a6ae1a7ea139c0c35d79f8ed0
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-16 02:11:48 +0300

    helloNew

--- telebot/entities/user.py
+++ telebot/entities/user.py
@@ -7,7 +7,7 @@ class User:
         self.session = requests.session()
         self.credentials = data.get('credentials')
         self.url = data.get('url')
-        self.region = data.get('region')
+        self.path = data.get('path')
         self.secret_key = None
 
     def authenticate(self):
@@ -17,14 +17,12 @@ class User:
         except Exception:
             return False
         result_json = result.json()
-        print(result_json)
         self.secret_key = result_json.get('security_token')
-        print(self.secret_key)
         return True if result_json.get('status') == 1 else False
 
     def get_dictionary(self):
         return {
             'url': self.url,
             'credentials': self.credentials,
-            'region': self.region
+            'path': self.path
         }
