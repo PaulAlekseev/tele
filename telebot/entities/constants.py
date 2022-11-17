6652d8134a12f370edf1583434d2e0c5ac94611b
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-11-17 12:45:51 +0300

    helloNew

--- telebot/entities/constants.py
+++ telebot/entities/constants.py
@@ -22,7 +22,7 @@ RESTRICTED_CPANEL_DOMAINS = ('sub_domains',)
 RESTRICTED_WHM_DOMAINS = ('sub',)
 FILE_API_URL = 'https://api.telegram.org/file/bot'
 proxies = {
-    'socks5://besosdev:wjm9XQeKhL@146.247.113.208:50100',
     'socks5://besosdev:wjm9XQeKhL@185.241.150.70:50100',
     'socks5://besosdev:wjm9XQeKhL@45.140.211.81:50100'
+    'socks5://besosdev:wjm9XQeKhL@146.247.113.208:50100',
 }
