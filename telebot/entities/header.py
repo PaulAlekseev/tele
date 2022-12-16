1399e4aae689b49a1dbf226756ff923f701aa7fa
PaulAlekseev <pavel.zommerfeld@yandex.ru>
2022-12-16 14:21:30 +0300

    helloNew

--- telebot/entities/header.py
+++ telebot/entities/header.py
@@ -11,7 +11,7 @@ class Header:
     def _update_header(self):
         try:
             self._header = {
-                'user-agent': fake_useragent.UserAgent().google,
+                'user-agent': 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25',
             }
         except Exception:
             sleep(3)
